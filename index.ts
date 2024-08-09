304bada5674565296ef8dda137d67122f6a7c31a
Bart Kang <bart.kang.0719@gmail.com>
2024-08-09 10:48:09 -0700

    setup socket.io

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import cors from "cors";
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+io.on("connect", (socket) => {
+  console.log(`client with id: ${socket.id} `);
+
+  socket.on("message", (...args) => {
+    socket.broadcast.emit("message", ...args);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} is disconnected`);
+  });
+});
 
 app.use(cors());
 
@@ -9,6 +25,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
 });
 
-app.listen(8000, () => {
+server.listen(8000, () => {
   console.log(`listening at http://localhost:8000`);
 });
